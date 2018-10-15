1013fe524d88f208ebb1f32851ef27e317eabf4d
Mitchel Kelonye <kelonyemitchel@gmail.com>
2018-10-15 12:58:20 +0200

    Rename to .js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { Api, Rpc, SignatureProvider } from 'eosjs'; // https://github.com/EOSIO/eosjs
-import { TextDecoder, TextEncoder } from 'text-encoding';
+import React, { Component } from "react";
+import { Api, Rpc, SignatureProvider } from "eosjs"; // https://github.com/EOSIO/eosjs
+import { TextDecoder, TextEncoder } from "text-encoding";
 
 // material-ui dependencies
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
 
 // eosio endpoint
 const endpoint = "http://localhost:8888";
@@ -19,40 +19,67 @@ const endpoint = "http://localhost:8888";
 // NEVER store private keys in any source code in your real life development
 // This is for demo purposes only!
 const accounts = [
-  {"name":"useraaaaaaaa", "privateKey":"5K7mtrinTFrVTduSxizUc5hjXJEtTjVTsqSHeBHes1Viep86FP5", "publicKey":"EOS6kYgMTCh1iqpq9XGNQbEi8Q6k5GujefN9DSs55dcjVyFAq7B6b"},
-  {"name":"useraaaaaaab", "privateKey":"5KLqT1UFxVnKRWkjvhFur4sECrPhciuUqsYRihc1p9rxhXQMZBg", "publicKey":"EOS78RuuHNgtmDv9jwAzhxZ9LmC6F295snyQ9eUDQ5YtVHJ1udE6p"},
-  {"name":"useraaaaaaac", "privateKey":"5K2jun7wohStgiCDSDYjk3eteRH1KaxUQsZTEmTGPH4GS9vVFb7", "publicKey":"EOS5yd9aufDv7MqMquGcQdD6Bfmv6umqSuh9ru3kheDBqbi6vtJ58"},
-  {"name":"useraaaaaaad", "privateKey":"5KNm1BgaopP9n5NqJDo9rbr49zJFWJTMJheLoLM5b7gjdhqAwCx", "publicKey":"EOS8LoJJUU3dhiFyJ5HmsMiAuNLGc6HMkxF4Etx6pxLRG7FU89x6X"},
-  {"name":"useraaaaaaae", "privateKey":"5KE2UNPCZX5QepKcLpLXVCLdAw7dBfJFJnuCHhXUf61hPRMtUZg", "publicKey":"EOS7XPiPuL3jbgpfS3FFmjtXK62Th9n2WZdvJb6XLygAghfx1W7Nb"},
-  {"name":"useraaaaaaaf", "privateKey":"5KaqYiQzKsXXXxVvrG8Q3ECZdQAj2hNcvCgGEubRvvq7CU3LySK", "publicKey":"EOS5btzHW33f9zbhkwjJTYsoyRzXUNstx1Da9X2nTzk8BQztxoP3H"},
-  {"name":"useraaaaaaag", "privateKey":"5KFyaxQW8L6uXFB6wSgC44EsAbzC7ideyhhQ68tiYfdKQp69xKo", "publicKey":"EOS8Du668rSVDE3KkmhwKkmAyxdBd73B51FKE7SjkKe5YERBULMrw"}
+  {
+    name: "useraaaaaaaa",
+    privateKey: "5K7mtrinTFrVTduSxizUc5hjXJEtTjVTsqSHeBHes1Viep86FP5",
+    publicKey: "EOS6kYgMTCh1iqpq9XGNQbEi8Q6k5GujefN9DSs55dcjVyFAq7B6b"
+  },
+  {
+    name: "useraaaaaaab",
+    privateKey: "5KLqT1UFxVnKRWkjvhFur4sECrPhciuUqsYRihc1p9rxhXQMZBg",
+    publicKey: "EOS78RuuHNgtmDv9jwAzhxZ9LmC6F295snyQ9eUDQ5YtVHJ1udE6p"
+  },
+  {
+    name: "useraaaaaaac",
+    privateKey: "5K2jun7wohStgiCDSDYjk3eteRH1KaxUQsZTEmTGPH4GS9vVFb7",
+    publicKey: "EOS5yd9aufDv7MqMquGcQdD6Bfmv6umqSuh9ru3kheDBqbi6vtJ58"
+  },
+  {
+    name: "useraaaaaaad",
+    privateKey: "5KNm1BgaopP9n5NqJDo9rbr49zJFWJTMJheLoLM5b7gjdhqAwCx",
+    publicKey: "EOS8LoJJUU3dhiFyJ5HmsMiAuNLGc6HMkxF4Etx6pxLRG7FU89x6X"
+  },
+  {
+    name: "useraaaaaaae",
+    privateKey: "5KE2UNPCZX5QepKcLpLXVCLdAw7dBfJFJnuCHhXUf61hPRMtUZg",
+    publicKey: "EOS7XPiPuL3jbgpfS3FFmjtXK62Th9n2WZdvJb6XLygAghfx1W7Nb"
+  },
+  {
+    name: "useraaaaaaaf",
+    privateKey: "5KaqYiQzKsXXXxVvrG8Q3ECZdQAj2hNcvCgGEubRvvq7CU3LySK",
+    publicKey: "EOS5btzHW33f9zbhkwjJTYsoyRzXUNstx1Da9X2nTzk8BQztxoP3H"
+  },
+  {
+    name: "useraaaaaaag",
+    privateKey: "5KFyaxQW8L6uXFB6wSgC44EsAbzC7ideyhhQ68tiYfdKQp69xKo",
+    publicKey: "EOS8Du668rSVDE3KkmhwKkmAyxdBd73B51FKE7SjkKe5YERBULMrw"
+  }
 ];
 // set up styling classes using material-ui "withStyles"
 const styles = theme => ({
   card: {
-    margin: 20,
+    margin: 20
   },
   paper: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
   },
   formButton: {
     marginTop: theme.spacing.unit,
-    width: "100%",
+    width: "100%"
   },
   pre: {
     background: "#ccc",
     padding: 10,
-    marginBottom: 0,
-  },
+    marginBottom: 0
+  }
 });
 
 // Index component
 class Index extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       noteTable: [] // to store the table rows from smart contract
     };
@@ -80,7 +107,7 @@ class Index extends Component {
         actionName = "update";
         actionData = {
           _user: account,
-          _note: note,
+          _note: note
         };
         break;
       default:
@@ -90,27 +117,39 @@ class Index extends Component {
     // eosjs function call: connect to the blockchain
     const rpc = new Rpc.JsonRpc(endpoint);
     const signatureProvider = new SignatureProvider([privateKey]);
-    const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+    const api = new Api({
+      rpc,
+      signatureProvider,
+      textDecoder: new TextDecoder(),
+      textEncoder: new TextEncoder()
+    });
     try {
-      const result = await api.transact({
-        actions: [{
-          account: "notechainacc",
-          name: actionName,
-          authorization: [{
-            actor: account,
-            permission: 'active',
-          }],
-          data: actionData,
-        }]
-      }, {
-        blocksBehind: 3,
-        expireSeconds: 30,
-      });
+      const result = await api.transact(
+        {
+          actions: [
+            {
+              account: "notechainacc",
+              name: actionName,
+              authorization: [
+                {
+                  actor: account,
+                  permission: "active"
+                }
+              ],
+              data: actionData
+            }
+          ]
+        },
+        {
+          blocksBehind: 3,
+          expireSeconds: 30
+        }
+      );
 
       console.log(result);
       this.getTable();
     } catch (e) {
-      console.log('Caught exception: ' + e);
+      console.log("Caught exception: " + e);
     }
   }
 
@@ -118,13 +157,15 @@ class Index extends Component {
   // and saves it into the component state: "noteTable"
   getTable() {
     const rpc = new Rpc.JsonRpc(endpoint);
-    rpc.get_table_rows({
-      "json": true,
-      "code": "notechainacc",   // contract who owns the table
-      "scope": "notechainacc",  // scope of the table
-      "table": "notestruct",    // name of the table as specified by the contract abi
-      "limit": 100,
-    }).then(result => this.setState({ noteTable: result.rows }));
+    rpc
+      .get_table_rows({
+        json: true,
+        code: "notechainacc", // contract who owns the table
+        scope: "notechainacc", // scope of the table
+        table: "notestruct", // name of the table as specified by the contract abi
+        limit: 100
+      })
+      .then(result => this.setState({ noteTable: result.rows }));
   }
 
   componentDidMount() {
@@ -142,17 +183,20 @@ class Index extends Component {
           <Typography variant="headline" component="h2">
             {user}
           </Typography>
-          <Typography style={{fontSize:12}} color="textSecondary" gutterBottom>
-            {new Date(timestamp*1000).toString()}
+          <Typography
+            style={{ fontSize: 12 }}
+            color="textSecondary"
+            gutterBottom
+          >
+            {new Date(timestamp * 1000).toString()}
           </Typography>
-          <Typography component="pre">
-            {note}
-          </Typography>
+          <Typography component="pre">{note}</Typography>
         </CardContent>
       </Card>
     );
     let noteCards = noteTable.map((row, i) =>
-      generateCard(i, row.timestamp, row.user, row.note));
+      generateCard(i, row.timestamp, row.user, row.note)
+    );
 
     return (
       <div>
@@ -193,20 +237,22 @@ class Index extends Component {
               variant="contained"
               color="primary"
               className={classes.formButton}
-              type="submit">
+              type="submit"
+            >
               Add / Update note
             </Button>
           </form>
         </Paper>
         <pre className={classes.pre}>
-          Below is a list of pre-created accounts information for add/update note:
-          <br/><br/>
-          accounts = { JSON.stringify(accounts, null, 2) }
+          Below is a list of pre-created accounts information for add/update
+          note:
+          <br />
+          <br />
+          accounts = {JSON.stringify(accounts, null, 2)}
         </pre>
       </div>
     );
   }
-
 }
 
 export default withStyles(styles)(Index);
