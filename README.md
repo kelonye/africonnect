f98c388b81925989be23de2dc09bb2c0829a732f
Mitchel Kelonye <kelonyemitchel@gmail.com>
2018-10-15 14:37:46 +0200

    Set Chain Id

---
+++ README.md
@@ -0,0 +1,1 @@
+alias cleos='docker exec eosio_zebra_container /opt/eosio/bin/cleos -u http://localhost:8888'
--- frontend/src/eos.js
+++ frontend/src/eos.js
@@ -8,7 +8,9 @@ export default new class {
       blockchain: 'eos',
       host: 'localhost',
       port: 8888,
-      protocol: 'http'
+      protocol: 'http',
+      chainId:
+        'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
     };
 
     document.addEventListener('scatterLoaded', scatterExtension => {
